f2d2a5f9b462ca52656c7e9557c57d86cf9dcc5b
Eleasah Halsmer <eleasahjh@gmail.com>
2019-11-23 15:00:14 -0700

    simplifying

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const createError = require('http-errors');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const socketIO = require('socket.io')
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'build/index.html')
 
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = express()
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`listening on ${PORT}`))
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+const io = socketIO(app)
+
+
 
 io.on('connection', (socket)=>{
   console.log('A user has connected');
@@ -25,18 +32,20 @@ io.on('connection', (socket)=>{
   });
 })
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*',(req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  .listen(port, ()=>console.log(`Server listening on port ${port}`));
-})
+// app.get('/*',(req, res) => {
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  // .listen(port, ()=>console.log(`Server listening on port ${port}`));
+// })
 
 // websockets
 // const io = socketIO(app);
@@ -47,19 +56,19 @@ app.get('/*',(req, res) => {
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
+// app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // res.status(err.status || 500);
+  // res.render('error');
+// });
 
 module.exports = app;
