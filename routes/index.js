e1579c498e62286324fddc53429b0d147d184f9b
Eleasah Halsmer <eleasahjh@gmail.com>
2019-11-23 11:58:09 -0700

    edits to app and / route in express server

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(express.static(path.join(__dirname, 'build')));
-
-
-router.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
